ce194c20cfa2c8c331c8792c20203137503a595d
Dejvis Sheshi <dejvis.sheshi96@gmail.com>
2024-01-10 15:33:42 +0100

    Add additional route that serves static web pages

--- routes.go
+++ routes.go
@@ -1,16 +1,34 @@
 package main
 
 import (
+	"bytes"
 	"encoding/json"
 	"fmt"
+	"html/template"
+	"io/ioutil"
 	"net/http"
+	"packSizeOptimizer/utils"
 	"strconv"
 	"strings"
 
+	"packSizeOptimizer/db"
 	"packSizeOptimizer/file"
 )
 
 var defaultItems = []int{250, 500, 1000, 2000, 5000}
+
+// PackageInterface is a custom interface for HTTP handlers.
+type PackageInterface interface {
+	RollbackPackageChanges(w http.ResponseWriter, r *http.Request)
+	AddNewPackages(w http.ResponseWriter, r *http.Request)
+	RemovePackages(w http.ResponseWriter, r *http.Request)
+}
+
+// HttpHandler is a type that implements MyHandlerInterface.
+type HttpHandler struct {
+	Repository db.PackagesRepository
+	UseFile    bool
+}
 
 // testHandler is a simple handler for the test endpoint
 func testHandler(w http.ResponseWriter, r *http.Request) {
@@ -18,7 +36,7 @@ func testHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 // RollbackPackageChanges is a handler for the rollback endpoint
-func RollbackPackageChanges(w http.ResponseWriter, r *http.Request) {
+func (h HttpHandler) RollbackPackageChanges(w http.ResponseWriter, r *http.Request) {
 	err := file.RollbackFileToInitialState("data.csv", defaultItems)
 	if err != nil {
 		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
@@ -46,7 +64,7 @@ func RollbackPackageChanges(w http.ResponseWriter, r *http.Request) {
 }
 
 // AddNewPackages is a handler for the add endpoint
-func AddNewPackages(w http.ResponseWriter, r *http.Request) {
+func (h HttpHandler) AddNewPackages(w http.ResponseWriter, r *http.Request) {
 	// Extract the "id" parameter from the URL
 	id := strings.TrimPrefix(r.URL.Path, "/add/")
 	id = strings.TrimSuffix(id, "/")
@@ -84,7 +102,7 @@ func AddNewPackages(w http.ResponseWriter, r *http.Request) {
 }
 
 // RemovePackages is a handler for the delete endpoint
-func RemovePackages(w http.ResponseWriter, r *http.Request) {
+func (h HttpHandler) RemovePackages(w http.ResponseWriter, r *http.Request) {
 	// Extract the "id" parameter from the URL
 	id := strings.TrimPrefix(r.URL.Path, "/remove/")
 	id = strings.TrimSuffix(id, "/")
@@ -121,6 +139,29 @@ func RemovePackages(w http.ResponseWriter, r *http.Request) {
 	w.Write(responseJSON)
 }
 
+// ReadPackages is a handler for the read endpoint
+func (h HttpHandler) ReadPackages(w http.ResponseWriter, r *http.Request) {
+	numbers, err := file.ReadNumbersFromCSV("data.csv")
+	if err != nil {
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
+	}
+
+	// Convert the response to JSON
+	responseJSON, err := json.Marshal(numbers)
+	if err != nil {
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
+	}
+
+	// Set the Content-Type header to application/json
+	w.Header().Set("Content-Type", "application/json")
+
+	// Write the JSON response to the client
+	w.Write(responseJSON)
+
+}
+
 // CalculatePackages is a handler for the Calculate endpoint
 func CalculatePackages(w http.ResponseWriter, r *http.Request) {
 	// Extract the "id" parameter from the URL
@@ -133,7 +174,8 @@ func CalculatePackages(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	packages := Calculate(itemsOrdered, defaultItems)
+	calculator := SingletonCalculator{}
+	packages := calculator.GetInstance().Calculate(defaultItems, itemsOrdered)
 	// Create a response JSON
 	response := packages
 
@@ -149,4 +191,134 @@ func CalculatePackages(w http.ResponseWriter, r *http.Request) {
 
 	// Write the JSON response to the client
 	w.Write(responseJSON)
+}
+
+// CalculateData is a handler for the visual Calculate endpoint. It takes packetSizes and items as input and returns
+// the combination as JSON response
+func CalculateData(w http.ResponseWriter, r *http.Request) {
+	// Read the request body
+	if r.Method != http.MethodPost {
+		http.Error(w, "Invalid request method", http.StatusMethodNotAllowed)
+		return
+	}
+
+	var requestData SubmitRequest
+	decoder := json.NewDecoder(r.Body)
+	err := decoder.Decode(&requestData)
+	if err != nil {
+		http.Error(w, "Error decoding JSON data", http.StatusBadRequest)
+		return
+	}
+
+	calculator := SingletonCalculator{}
+	result := calculator.GetInstance().Calculate(requestData.PackSizes, requestData.Items)
+	responseJSON, err := json.Marshal(result)
+	if err != nil {
+		http.Error(w, "Error creating response JSON", http.StatusInternalServerError)
+		return
+	}
+
+	// Set the Content-Type header to application/json
+	w.Header().Set("Content-Type", "application/json")
+
+	// Write the response JSON to the response writer
+	w.Write(responseJSON)
+}
+
+func CalculateTemplate(w http.ResponseWriter, r *http.Request) {
+	// Parse the HTML template
+	tmpl, err := template.ParseFiles("templates/calculate_form.html")
+
+	if err != nil {
+		http.Error(w, "Error parsing HTML template", http.StatusInternalServerError)
+		return
+	}
+
+	// Execute the template with the data and write to the response
+	err = tmpl.Execute(w, nil)
+	if err != nil {
+		http.Error(w, "Error executing HTML template", http.StatusInternalServerError)
+		return
+	}
+}
+
+type SubmitRequest struct {
+	PackSizes []int `json:"packSizes"`
+	Items     int   `json:"items"`
+}
+
+func SubmitHandler(w http.ResponseWriter, r *http.Request) {
+	packs := r.FormValue("packSizes")
+	packs = strings.ReplaceAll(packs, " ", "")
+
+	packSizes, err := utils.ConvertStringToArray(packs)
+	if err != nil {
+		http.Error(w, "Invalid input", http.StatusBadRequest)
+		return
+	}
+
+	if len(packSizes) == 0 {
+		http.Error(w, "Invalid input", http.StatusBadRequest)
+		return
+	}
+
+	itemsStr := r.FormValue("items")
+	items, err := strconv.Atoi(itemsStr)
+	if err != nil {
+		http.Error(w, "Invalid input", http.StatusBadRequest)
+		return
+	}
+
+	request := SubmitRequest{
+		PackSizes: packSizes,
+		Items:     items,
+	}
+
+	jsonData, err := json.Marshal(request)
+	if err != nil {
+		http.Error(w, "Failed to marshall structure", http.StatusInternalServerError)
+		return
+	}
+
+	response, err := http.Post("http://localhost:8080/form/calculate", "application/json", bytes.NewBuffer(jsonData))
+	if err != nil {
+		fmt.Println("Error making HTTP request:", err)
+		return
+	}
+	defer response.Body.Close() // Close the response body when done
+
+	// Read the response body
+	body, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		fmt.Println("Error reading response body:", err)
+		return
+	}
+	fmt.Println("Submitted body: ", string(body))
+
+	var packagesInfo []PackageInfo
+	err = json.Unmarshal(body, &packagesInfo)
+	if err != nil {
+		fmt.Println("Error unmarshaling JSON:", err)
+		return
+	}
+
+	data := struct {
+		Packages []PackageInfo
+	}{
+		Packages: packagesInfo,
+	}
+
+	// Parse the HTML template
+	tmpl, err := template.ParseFiles("templates/submit.html")
+	if err != nil {
+		http.Error(w, "Error parsing HTML template", http.StatusInternalServerError)
+		return
+	}
+
+	// Execute the template with the data and write to the response
+	err = tmpl.Execute(w, data)
+	if err != nil {
+		http.Error(w, "Error executing HTML template", http.StatusInternalServerError)
+		return
+	}
 }
--- utils/utils.go
+++ utils/utils.go
@@ -1,1 +1,29 @@
 package utils
+
+import (
+	"fmt"
+	"strconv"
+	"strings"
+)
+
+func ConvertStringToArray(input string) ([]int, error) {
+	// Remove spaces from the input string
+	input = strings.ReplaceAll(input, " ", "")
+
+	// Split the input string by commas
+	strNumbers := strings.Split(input, ",")
+
+	// Initialize an array to store the converted numbers
+	var numbers []int
+
+	// Iterate through the split strings and convert each to an integer
+	for _, strNum := range strNumbers {
+		num, err := strconv.Atoi(strNum)
+		if err != nil {
+			return nil, fmt.Errorf("failed to convert string to number: %v", err)
+		}
+		numbers = append(numbers, num)
+	}
+
+	return numbers, nil
+}
